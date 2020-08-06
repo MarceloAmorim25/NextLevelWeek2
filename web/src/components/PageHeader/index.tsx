288cc784e0a370fe2a869889e64718c8793f4aed
Marcelo Amorim <marcelo.med.amorim@gmail.com>
2020-08-06 00:24:40 -0300

    Adiciona Dia 3

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import backIcon from '../../assets/images/icons/back.svg'
 import logoImg from '../../assets/images/logo.svg'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 import './styles.css'
 
 interface PageHeaderProps {
     title: string;
+    description?: string;
 }
 
 const PageHeader: React.FC<PageHeaderProps> = (props) => {
@@ -24,7 +25,11 @@ const PageHeader: React.FC<PageHeaderProps> = (props) => {
 
         <div className="header-content">
             <strong>{props.title}</strong>
+
+            {props.description && <p>{props.description}</p>}
+
             {props.children}
+
        </div>
        
     </header>
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -4,6 +4,8 @@ import './styles.css'
 
 import PageHeader from '../../components/PageHeader'
 import TeacherItem from '../../components/TeacherItem'
+import Input from '../../components/Input'
+import Select from '../../components/Select'
 
 const TeacherList = () => {
     return(
@@ -12,20 +14,34 @@ const TeacherList = () => {
                 
             <form id="search-teachers">
 
-                    <div className="input-block">
-                        <label htmlFor="subject">Matéria</label>
-                        <input type="text" id="subject"/>
-                    </div>
+                    <Select 
+                        name="subject" 
+                        label="Matéria"
+                        options={[
+                            {value: 'Artes', label: 'Artes'},
+                            {value: 'Biologia', label: 'Biologia'},
+                            {value: 'Matemática', label: 'Matemática'},
+                            {value: 'Português', label: 'Português'},
+                            {value: 'História', label: 'História'},
+                            {value: 'Geografia', label: 'Geografia'},                   
+                        ]}
+                    />
 
-                    <div className="input-block">
-                        <label htmlFor="subject">Dia de semana</label>
-                        <input type="text" id="week_day"/>
-                    </div>
-
-                    <div className="input-block">
-                        <label htmlFor="subject">Hora</label>
-                        <input type="text" id="time"/>
-                    </div>
+                <Select 
+                        name="week_day" 
+                        label="Dia da Semana"
+                        options={[
+                            {value: '0', label: 'Domingo'},
+                            {value: '1', label: 'Segunda-feira'},
+                            {value: '2', label: 'Terça-feira'},
+                            {value: '3', label: 'Quarta-feira'},
+                            {value: '4', label: 'Quinta-feira'},
+                            {value: '5', label: 'Sexta-feira'},                   
+                            {value: '6', label: 'Sábado'},                   
+                        ]}
+                    />
+                
+                <Input type="time" name="time" label="Hora" />
 
             </form> 
 
